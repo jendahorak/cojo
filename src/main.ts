bd1dbd74f18b8f73543f935d483af2cde1f0e10c
jenda_horak <484526@muni.cz>
2023-10-01 08:57:16 +0200

    First two excercises

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,29 @@
-import { fn } from './module.ts';
+// type F = (x: number) => number;
 
-console.log(fn(1));
+// function compose(functions: F[]): F {
+//   return function (x) {
+//     for (let i = functions.length - 1; i >= 0; i--) {
+//       x = functions[i](x);
+//     }
+//     return x;
+//   };
+// }
+
+// /**
+//  * const fn = compose([x => x + 1, x => 2 * x])
+//  * fn(4) // 9
+//  */
+
+// const fn = compose([(x) => x + 1, (x) => 2 * x]);
+// console.log(fn(4));
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+  return args.length;
+}
+
+/**
+ * ; // 3
+ */
+console.log(argumentsLength([1, 2, 3, 'prdel', { asdf: 'kas' }]));
--- src/module.ts
+++ src/module.ts
@@ -1,3 +1,11 @@
-export function fn(arg: any) {
-  return arg;
-}
+// export type F = (x: number) => number;
+// function compose(functions: F[]): F {
+//   return function (x) {
+
+//   };
+// }
+
+// /**
+//  * const fn = compose([x => x + 1, x => 2 * x])
+//  * fn(4) // 9
+//  */
